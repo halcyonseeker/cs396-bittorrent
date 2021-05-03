12b99798b5736d07414251c43076500f478e9b67
Thalia Wright <vesperous@protonmail.com>
2021-05-02 21:08:18 -0700

    We need more fields for interacting with the trackers and peers

--- bitclient.h
+++ bitclient.h
@@ -19,28 +19,51 @@
 
 extern int log_verbosely;
 
-/**
- * Torrent files are passed on the command line, we'll store them in a
- * linked list of torrent_t structs. The data field is a pointer to the
- * data from the .torrent file and the other fields contain the bencoded
- * information that we'll actually use. The chunks_t struct contains the
- * the information required to validate and serialize the data we get
- * from peers.
- */
 typedef long long int be_num_t;
 
+/* Store information about the peers */
+typedef struct peers {
+    char  *id;           /* The peer's self-selected id */
+    char  *ip;           /* Duh */
+    char  *port;         /* Duh */
+    struct peers *next;  /* I'm tired of linked lists */
+} peers_t;
+
+/* Store the tracker's response */
+typedef struct tracker {
+    char *   id;         /* A string I should use to identify myself */
+    be_num_t interval;   /* Number of seconds to wait before sending a request */
+    be_num_t complete;   /* Number of peers with full file */
+    be_num_t leechers;   /* Number of peers with incomplete file */
+    peers_t *peers;      /* A linked list of peer info */
+} tracker_t;
+
+/* Store a torrent's piece checksums */
 typedef struct chunk {
     be_num_t       num;
     char         *checksum;
     struct chunk *next;
 } chunk_t;
 
+/* Store information about the torrent specified on the command line */
 typedef struct torrent {
-    be_node_t *     data;
-    be_num_t        piece_len;
-    be_num_t        file_len;
-    chunk_t *       pieces;
-    char *          filename;
-    char *          announce;
-    struct torrent *next;
+    be_node_t *     data;      /* Decoded metainfo file */
+    /* Information from the metainfo file */
+    be_num_t        piece_len; /* The length of pieces the file is split into */
+    be_num_t        file_len;  /* The lendth of the file we're downloading */
+    chunk_t *       pieces;    /* A linked list of piece numbers and hashes */
+    char *          filename;  /* The name of the file we should save */
+    char *          announce;  /* The tracker URL. We only support 1 for now */
+    char *          info_hash; /* A sha1 hash of the pieces hash string */
+    /* Look Ma, I'm a peer now! */
+    char *          peer_id;   /* Who am I? No seriously, who am I really? */
+    char *          port;      /* The port we'll listen on */
+    char *          event;     /* What we're doing rn */
+    be_num_t        uploaded;  /* How many bytes we've uploaded */
+    be_num_t        dloaded;   /* How many bytes we've downloaded */
+    be_num_t        left;      /* The number of bytes we still need */
+    be_num_t        compact;   /* No */
+    /* Information from the announce tracker */
+    tracker_t       trackers;  /* Some nice folks we should talk with */
+    struct torrent *next;      /* We shouldn't support multiple torrents */
 } torrent_t;
