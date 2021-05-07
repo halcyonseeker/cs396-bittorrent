541eea67d06b90834d49ba9d67dc4e94c97b71fb
Thalia Wright <vesperous@protonmail.com>
2021-05-07 10:09:16 -0700

    Added itoa

--- magnet.c
+++ magnet.c
@@ -33,6 +33,43 @@
 
 
 /**
+ * Ansi C "itoa" based on Kernighan & Ritchie's "Ansi C":
+ * Sourced from http://www.strudel.org.uk/itoa/
+ * These are used for debugging in the packet generating functions
+ */
+
+static void
+_strreverse(char* begin, char* end) {
+    char aux;
+    while(end>begin)
+        aux=*end, *end--=*begin, *begin++=aux;
+}
+
+static void
+_itoa(long long value, char* str, int base) {
+    static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    char* wstr=str;
+    long long sign;
+
+    // Validate base
+    if (base<2 || base>35){ *wstr='\0'; return; }
+
+    // Take care of sign
+    if ((sign=value) < 0) value = -value;
+
+    // Conversion. Number is reversed.
+    do *wstr++ = num[value%base]; while(value/=base);
+
+    if(sign<0) *wstr++='-';
+    *wstr='\0';
+
+    // Reverse string
+    _strreverse(str,wstr-1);
+}
+
+
+
+/**
  * Base conversion is fiddly and error-prone so I borrowed this from here:
  * github.com/transmission/transmission/blob/master/libtransmission/utils.c#L815
  */
